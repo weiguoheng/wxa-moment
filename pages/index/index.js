9e790458eb21bee92f6ee0b492349956993d0cd5
weiguoheng <weiguoheng1119@163.com>
2018-04-29 18:12:08 +0800

    Update index.js
    新增获取目标时间

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,17 @@ Page({
     const time1='2018-03-31T07:10:56.000Z',
       time2 = '2018-04-02T07:10:56.000Z',
       time3 = '2018-03-31T07:11:56.000Z',
-      time4= '4852'
+      time4= '4852',
+      time5= '2018-05-05'
     const format_time = moment.format(time1,'YYYY-MM-DD hh:mm:ss');
     const dif_time = moment.dif(time2,time3);
     const second_str = moment.secondToCountdown(time4,'DD天hh小时mm分钟ss秒');
+    const aimsTime = moment.getAimsDate(time5,5,'minute');
     this.setData({
       format_time: format_time,
       dif_time: dif_time,
-      second_str: second_str
+      second_str: second_str,
+      aimsTime:aimsTime
     })
   }
 })
