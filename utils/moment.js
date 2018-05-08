9e27138e7ae3607dc5d0d09671527137d48765e2
weiguoheng <weiguoheng1119@163.com>
2018-05-08 10:26:00 +0800

    Update moment.js

--- utils/moment.js
+++ utils/moment.js
@@ -1,5 +1,5 @@
 module.exports = {
-  format: function (d, str) {
+  format: function(d, str) {
     if (!d) {
       return;
     }
@@ -24,23 +24,33 @@ module.exports = {
       .replace("ss", ss);
   },
   // 时间戳转时间
-  timestampToTime(timestamp) {
-        const date = new Date(timestamp * 1000),//时间戳为10位需*1000，时间戳为13位的话不需乘1000
-        Y = date.getFullYear() + '-',
-        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-',
-        D = date.getDate() + ' ',
-        h = date.getHours() + ':',
-        m = date.getMinutes() + ':',
-        s = date.getSeconds()
-        return Y+M+D+h+m+s;
-    },
+  timestampToTime(timestamp, str = "YYYY-MM-DD hh:mm:ss") {
+    const date = new Date(timestamp * 1000), //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      YYYY = date.getFullYear(),
+      MM = date.getMonth() + 1 < 10
+        ? "0" + (date.getMonth() + 1)
+        : date.getMonth() + 1,
+      DD = date.getDate() < 10 ? "0" + date.getDate() : date.getDate(),
+      hh = " " + date.getHours(),
+      mm = date.getMinutes(),
+      ss = date.getSeconds();
+    if (str.indexOf(str, "hh") != -1) {
+      return str
+        .replace("YYYY", YYYY)
+        .replace("MM", MM)
+        .replace("DD", DD)
+        .replace("hh", hh)
+        .replace("mm", mm)
+        .replace("ss", ss);
+    } else {
+      return str.replace("YYYY", YYYY).replace("MM", MM).replace("DD", DD);
+    }
+  },
   // 计算时间差
   dif(start, end, type) {
     const start_time = this.format(start, "YYYY-MM-DD hh:mm:ss");
     const end_time = this.format(end, "YYYY-MM-DD hh:mm:ss");
-    const start_time_date = new Date(
-      start_time.replace(/\-/g, "/")
-    ).getTime();
+    const start_time_date = new Date(start_time.replace(/\-/g, "/")).getTime();
     const end_time_date = new Date(end_time.replace(/\-/g, "/")).getTime();
     let timeType = 0;
     switch (type) {
@@ -63,8 +73,7 @@ module.exports = {
     let time_dif = parseInt(end_time_date) - parseInt(start_time_date);
     if (timeType) {
       time_dif =
-        (parseInt(start_time_date) - parseInt(end_time_date)) /
-        timeType;
+        (parseInt(start_time_date) - parseInt(end_time_date)) / timeType;
     } else {
       let dif_date = parseInt(start_time_date) - parseInt(end_time_date);
       const dif_day = parseInt(dif_date / (1000 * 3600 * 24));
@@ -101,7 +110,7 @@ module.exports = {
     return countdown;
   },
   // 获取时间
-  getAimsDate(date, time = 0, time_type = "day") {
+  getAimsDate(date, time = 0, str = "YYYY-MM-DD hh:mm:ss", time_type = "day") {
     const start_date = new Date(date.replace(/\-/g, "/")).getTime();
     let timeType = 0;
     switch (time_type) {
@@ -121,8 +130,8 @@ module.exports = {
         timeType = 0;
         break;
     }
-    const aims_date = start_date + timeType * time;
-    var date = this.timestampToTime(aims_date / 1000);
-    return date;
+    const aims_stamp = start_date + timeType * time;
+    const aims_date = this.timestampToTime(aims_stamp / 1000, str);
+    return aims_date;
   }
 };
